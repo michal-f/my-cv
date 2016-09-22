e921ac352770144fa85d2e7c041bb7f115da6835
root <root@mmp.pl>
2016-09-22 15:27:46 +0200

    Initial commit

---
+++ js/map.js
@@ -0,0 +1,17 @@
+//alert("sss");
+////function myMap() {
+////  var mapCanvas = document.getElementById("map");
+////  var myCenter = new google.maps.LatLng(51.508742,-0.120850);
+////  var mapOptions = {center: myCenter, zoom: 5};
+////  var map = new google.maps.Map(mapCanvas,mapOptions);
+////  var marker = new google.maps.Marker({
+////    position: myCenter,
+////    icon: "http://icons.iconarchive.com/icons/custom-icon-design/pretty-office-8/128/Accept-icon.png",
+////    animation: google.maps.Animation.BOUNCE
+////  });
+////  marker.setMap(map);
+////}
+//    $(function(){
+//        alert("aaa");
+//      $("#includedContent").load("../map.html");
+//    });
---
+++ js/my_cv.js
@@ -0,0 +1,101 @@
+/**
+ * Created by M.Frackowiak on 01.07.2016.
+ */
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+var div_list = ["#home", "#resume", "#profile", "#portfolio", "#blog", "#contact", "#my-profile", "#blog-section"];
+var animation_class_pairs = [
+
+    //["slideDownRetourn", "vanishOut"],
+    ["vanishIn", "vanishOut"],
+    //["puffIn", "puffOut"],
+    //["vanishIn", "vanishOut"],
+    //["spaceInUp", "spaceOutUp"],
+    //["spaceInRight", "spaceOutRight"],
+    //["spaceInDown", "spaceOutDown"],
+    //["spaceInLeft", "spaceOutLeft"],
+    //["boingInUp", "boingOutDown"],
+    //["foolishIn", "foolishOut"],
+    //["perspectiveDownRetourn", "perspectiveDown"],
+    //["perspectiveLeftRetourn", "perspectiveLeft"],
+    //["perspectiveRightRetourn", "perspectiveRight"],
+    //["slideDownRetourn", "slideDown"],
+    //["slideUpRetourn", "slideUp"],
+    //["openDownLeftRetourn", "openDownLeft"],
+    //["openDownRightRetourn", "openDownRight"],
+    //["openUpLeftRetourn", "openUpLeft"],
+    //["openUpRightRetourn", "openUpRight"],
+];
+//#### MENU ITEM LIST #####
+var currentDiv = "home";
+var currentMenuItem = $("#menu-menu-1 > li > a:nth-child(7)");
+var clickedMenuItem;
+var blockChanging = false;
+
+function change_div(clicked) {
+    blockChanging = true;
+    $("#menu-menu-1 > li > a").addClass("btntextgrey");
+    $("#mfstart").addClass("btntextgrey");
+    var menu_clicked = clicked;
+    currentDiv = menu_clicked;
+
+    /*RANDOMIZE ANIMATION*/
+    var random_int = Math.floor((Math.random() * animation_class_pairs.length));
+    var animation_in = animation_class_pairs[random_int][0];
+    var animation_out = animation_class_pairs[random_int][1];
+
+    /*CHECK VISIBLE AND CLICKED DIV */
+    var visible_div, visible_div_animate_out_class;
+    var clicked_div, clicked_div_animate_in_class;
+    for (i in div_list) {
+        if ($(div_list[i]).is(":visible")) {
+            visible_div = $(div_list[i]);
+            visible_div_animate_out_class = "magictime " + animation_out;
+        }
+        if (div_list[i].toLowerCase().substring(1) == menu_clicked) {
+            clicked_div = $(div_list[i]);
+            clicked_div_animate_in_class = "magictime " + animation_in;
+        }
+    }
+
+    /* TRANSFORM*/
+    visible_div.addClass(visible_div_animate_out_class);
+
+    /*TIMEOUT WAIT FUNCTION FOR DIV HIDING*/
+    setTimeout(function () {
+        for (i in div_list) {
+            $(div_list[i]).hide();
+        }
+        visible_div.removeClass(visible_div_animate_out_class);
+        clicked_div.addClass(clicked_div_animate_in_class);
+        clicked_div.show();
+    }, 800);
+    setTimeout(function () {
+        clicked_div.removeClass(clicked_div_animate_in_class);
+        blockChanging = false;
+        $("#menu-menu-1 > li > a").removeClass("btntextgrey");
+        $("#mfstart").removeClass("btntextgrey");
+        if (clicked_div.hasClass("contact"))
+            myMap();
+    }, 1700);
+}
+
+$("#menu-menu-1 > li > a").click(function () {
+    if (currentDiv != $(this).text().toLowerCase() && blockChanging == false) {
+        clickedMenuItem = $(this);
+        clickedMenuItem.addClass("btn-primary");
+        if (currentMenuItem.hasClass("btn-primary")) {
+            currentMenuItem.removeClass("btn-primary");
+        }
+        change_div(clickedMenuItem.text().toLowerCase());
+        currentMenuItem = clickedMenuItem;
+
+    }
+});
+$("#mfstart").click(function () {
+    if (blockChanging == false) {
+        change_div("home");
+        currentMenuItem.removeClass("btn-primary");
+    }
+});
