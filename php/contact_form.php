0ee91a551cc572f4a49d233a87aa0ed68cd35021
m.frackowiak <michal.f@mail.com>
2016-09-27 18:01:10 +0200

    contact form refactor

--- php/contact_form.php
+++ php/contact_form.php
@@ -13,20 +13,20 @@ if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
     $headers .= 'From:' . $email . "\r\n"; // Sender's Email
     $headers .= 'Cc:' . $email . "\r\n"; // Carbon copy to Sender
-    $template = '<div style="padding:50px; color:white;">Hello ' . $name . ',<br/>'
-        . '<br/>Thank you...! For Contacting Me.<br/><br/>'
+    $template = '<div style="padding:50px; color:white;">Hello ,<br/>'
+        . '<br/>Thank you for Contacting Me.<br/><br/>'
         . 'Name:' . $name . '<br/>'
         . 'Email:' . $email . '<br/>'
         . 'Contact No:' . $contact . '<br/>'
         . 'Message:' . $message . '<br/><br/>'
-        . 'This is a Contact Confirmation mail.'
+        . 'This is a Contact Form Confirmation mail from www.mfx.ovh'
         . '<br/>'
-        . 'I Will contact You as soon as possible .</div>';
+        . 'I Will contact You as soon as possible, <br/>Michał Frąckowiak.</div>';
     $sendmessage = "<div style=\"background-color:#7E7E7E; color:white;\">" . $template . "</div>";
 // Message lines should not exceed 70 characters (PHP rule), so wrap it.
     $sendmessage = wordwrap($sendmessage, 70);
 // Send mail by PHP Mail Function.
-    mail("buhahahaha@gmail.com", $subject, $sendmessage, $headers);
+    mail("michalfrackowiak@vp.pl", $subject, $sendmessage, $headers);
     mail($email, $subject, $sendmessage, $headers);
     echo "Your Query has been received, I will contact you soon.";
 } else {
